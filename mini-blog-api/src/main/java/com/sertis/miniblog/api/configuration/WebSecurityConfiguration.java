c60f0225b96560fb079948e0d41d3b35219f0eff
chiwa <chiwa@central.tech>
2021-03-13 15:44:56 +0700

    Added Jwt for User Authentication

---
+++ mini-blog-api/src/main/java/com/sertis/miniblog/api/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,65 @@
+package com.sertis.miniblog.api.configuration;
+
+
+
+import com.sertis.miniblog.api.security.JwtAuthenticationFilter;
+import com.sertis.miniblog.api.security.MiniBlogAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Resource(name = "userService")
+    private UserDetailsService userDetailsService;
+
+    private MiniBlogAuthenticationEntryPoint miniBlogAuthenticationEntryPoint;
+
+    @Autowired
+    public void setUnauthorizedHandler(MiniBlogAuthenticationEntryPoint unauthorizedHandler) {
+        this.miniBlogAuthenticationEntryPoint = unauthorizedHandler;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter authenticationTokenFilter() throws Exception {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/ping", "/h2/**", "/swagger*/**", "/webjars/**", "/v2/*",  "/login", "/blogs", "/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(miniBlogAuthenticationEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.headers().frameOptions().sameOrigin();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
---
+++ mini-blog-api/src/main/java/com/sertis/miniblog/api/security/JwtTokenService.java
@@ -0,0 +1,91 @@
+package com.sertis.miniblog.api.security;
+
+
+import com.sertis.miniblog.api.configuration.JwtConfiguration;
+import com.sertis.miniblog.api.repository.impl.UserServiceImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtTokenService implements Serializable {
+
+    private JwtConfiguration jwtConfiguration;
+    private UserServiceImpl userService;
+
+    @Autowired
+    public void setJwtConfiguration(JwtConfiguration jwtConfiguration) {
+        this.jwtConfiguration = jwtConfiguration;
+    }
+
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtConfiguration.getSigningKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(String subject) {
+        return doGenerateToken(subject);
+    }
+
+    private String doGenerateToken(String subject) {
+
+        Claims claims = Jwts.claims().setSubject(subject);
+        //claims.put("scopes", Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuer("http://www.sertis.co.th")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtConfiguration.getExpired() * 1000))
+                .signWith(SignatureAlgorithm.HS256, jwtConfiguration.getSigningKey())
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (
+              username.equals(userDetails.getUsername())
+                    && !isTokenExpired(token));
+    }
+
+    public com.sertis.miniblog.api.model.user.User getUserInformation(HttpServletRequest req) {
+        final String header = req.getHeader(jwtConfiguration.getHeader());
+        final String token = header.replace(jwtConfiguration.getPrefix(), "");
+        final String userName = getUsernameFromToken(token);
+        return userService.findByUserName(userName);
+    }
+
+}
