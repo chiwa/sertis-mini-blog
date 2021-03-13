cfd66bc477c04814260367b6e53040890bca7350
chiwa <chiwa@central.tech>
2021-03-13 15:35:10 +0700

    Added Authentication Request & Response Data Model

---
+++ mini-blog-api/src/main/java/com/sertis/miniblog/api/model/request/AuthenticationRequest.java
@@ -0,0 +1,23 @@
+package com.sertis.miniblog.api.model.request;
+
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ mini-blog-api/src/main/java/com/sertis/miniblog/api/model/response/LoginResponse.java
@@ -0,0 +1,40 @@
+package com.sertis.miniblog.api.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class LoginResponse {
+
+    public LoginResponse() {
+        super();
+    }
+    public void LoginResponse(com.sertis.miniblog.api.model.user.User user, String token) {
+        this.username = user.getUsername();
+        this.token = token;
+    }
+
+    @JsonProperty("user_name")
+    private String username;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("token")
+    private String token;
+
+    public  LoginResponse(com.sertis.miniblog.api.model.user.User user, String token) {
+        this.username = user.getUsername();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.token = token;
+
+    }
+}
