c4c5fff66acc320ac53e292876c2ed64dbb34c72
chiwa <chiwa@central.tech>
2021-03-13 15:23:35 +0700

    Added Swagger Configuration

--- mini-blog-api/src/main/java/com/sertis/miniblog/api/controller/UserController.java
+++ mini-blog-api/src/main/java/com/sertis/miniblog/api/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.sertis.miniblog.api.controller;
 
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +14,10 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class UserController {
 
-
+    @ApiOperation(value = "Ping/Pong API.", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "pong")
+    })
     @GetMapping(value = "/ping")
     public String ping(HttpServletRequest req){
         return "pong";
